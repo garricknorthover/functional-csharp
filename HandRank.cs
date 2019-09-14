7c2a1e8dd5fd52aac6f694ead68d212be7c3a76b
garricknorthover <garricknorthover@gmail.com>
2019-09-14 19:59:23 +1200

    add gitignore

---
+++ HandRank.cs
@@ -0,0 +1,13 @@
+public enum HandRank
+{
+    HighCard,
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush
+}
